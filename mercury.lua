0f860a36751a6f39030a499fae3a7d193607c615
Ramzesuskan <126349787+Ramzesuskan@users.noreply.github.com>
2025-11-07 22:07:43 +0100

    Update mercury.lua

--- mercury.lua
+++ mercury.lua
@@ -432,10 +432,10 @@ function Library:create(options)
 	end
 
 	options = self:set_defaults({
-		Name = "Mercury",
+		Name = "Moon",
 		Size = UDim2.fromOffset(600, 400),
 		Theme = self.Themes[settings.Theme],
-		Link = "https://github.com/deeeity/mercury-lib"
+		Link = "https://github.com/7g0d/moon-ware"
 	}, options)
 
 	if getgenv and getgenv().MercuryUI then
